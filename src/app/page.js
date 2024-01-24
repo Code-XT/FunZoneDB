444494b7f050b5c0e4281fb9b030014d4ad9c0e3
Code-XT <rahuljhabokaro123@gmail.com>
2024-01-24 21:20:40 +0530

    added homescreen animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative overflow-hidden">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/3 flex place-items-center before:absolute before:h-[400px] before:w-[600px] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[300px] after:w-[400px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[500px] z-[-1]"></div>
+    <main className="flex min-h-screen flex-col items-center justify-between relative p-5 overflow-hidden">
+      {" "}
+      <Spline scene="https://prod.spline.design/Y09oT1IYEuvZJFA0/scene.splinecode" />
     </main>
   );
 }
